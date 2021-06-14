7bad8ec0d8286ac9f41a1830d3be5f5242ff31c9
Min Gee <suh@student.unimelb.edu.au>
2021-06-14 22:04:13 +0900

    feat: type guard

---
+++ class-note/10_type-guard.ts
@@ -0,0 +1,26 @@
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+const introduce = (): Developer | Person => {
+    return {name: 'Min', age: 29, skill: 'typescript'}
+}
+
+const min = introduce();
+console.log(min.name);
+
+const isDeveloper = (target: Developer | Person): target is Developer => {
+    return (target as Developer).skill !== undefined;
+}
+
+if (isDeveloper(min)) {
+    console.log(min.skill)
+} else {
+    console.log(min.age)
+}
